c7b05150c330dde14398d1fb28f422310ac1995e
Sooraj-Rao <soorajrao630@gmail.com>
2025-06-08 12:36:21 +0530

    testing loc

--- src/app/api/loc/route.ts
+++ src/app/api/loc/route.ts
@@ -1,12 +1,14 @@
-// app/api/location/route.ts
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   try {
     const forwarded = req.headers.get("x-forwarded-for");
     const ip = forwarded?.split(",")[0] || "8.8.8.8";
-    const res = await fetch(`https://ipapi.co/${ip}/json`);
+    const url = `https://ipapi.co/${ip}/json`;
+    const res = await fetch(url);
     const data = await res.json();
+    console.log(ip);
+    console.log(url);
     console.log(data);
     return NextResponse.json(data);
   } catch (err) {

2c6437c643ef216816369fca8ccaf2f3659283c1
Sooraj-Rao <soorajrao630@gmail.com>
2025-06-08 12:26:08 +0530

    analytics testing

--- src/app/api/loc/route.ts
+++ src/app/api/loc/route.ts
@@ -3,16 +3,17 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   try {
-    // Extract user's IP from headers
     const forwarded = req.headers.get("x-forwarded-for");
-    const ip = forwarded?.split(",")[0] || "8.8.8.8"; // fallback IP (Google DNS for testing)
-
-    const res = await fetch(`https://ipinfo.io/${ip}/json`);
+    const ip = forwarded?.split(",")[0] || "8.8.8.8";
+    const res = await fetch(`https://ipapi.co/${ip}/json`);
     const data = await res.json();
-
-    return NextResponse.json({ ip, ...data });
+    console.log(data);
+    return NextResponse.json(data);
   } catch (err) {
     console.error("Failed to fetch location:", err);
-    return NextResponse.json({ error: "Location fetch failed" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Location fetch failed" },
+      { status: 500 }
+    );
   }
 }
